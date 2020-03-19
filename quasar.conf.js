6cc717a18b3788d945b94852d43b40c1048d31ee
Andrey Cherepanov <che1.pro@yandex.ru>
2020-03-20 00:07:36 +0700

    change main theme color

--- quasar.conf.js
+++ quasar.conf.js
@@ -121,7 +121,7 @@ module.exports = function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#4c6f6b',
         icons: [
           {
             'src': 'statics/icons/icon-128x128.png',
